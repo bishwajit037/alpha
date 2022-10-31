7dc88076f18cf44877d619759ff341b67ebe0e75
Bishwajit Roy <bishwajit.math.social@gmail.com>
2022-10-31 15:16:45 +0600

    Thirteen

--- single.php
+++ single.php
@@ -1,15 +1,6 @@
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
-<div class="header">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="tagline"><?php bloginfo("description") ?></h3>
-                <h1 class="align-self-center display-1 text-center heading"><a href="<?php echo get_site_url(); ?>"><?php bloginfo("name") ?></a></h1>
-            </div>
-        </div>
-    </div>
-</div>
+<?php get_template_part("hero"); ?>
 <div class="posts">
     <?php 
         while (have_posts()){
