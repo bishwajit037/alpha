601b4d6d91b4b3aba4fdbc60f68458086f0d9e82
Bishwajit Roy <bishwajit.social.math@gmail.com>
2022-10-31 23:22:52 +0600

    Footer Css Problem with Gutenberg

--- functions.php
+++ functions.php
@@ -11,3 +11,42 @@ function alpha_assets(){
     wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
 }
 add_action("wp_enqueue_scripts", "alpha_assets");
+
+function alpha_sidebar() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Single Post Sidebar', 'alpha' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Right Sidebar', 'alpha' ),
+			'before_widget' => '<div id="%1$s" class="sidebar-1 %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+        );
+
+    register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Right Sidebar', 'alpha' ),
+			'id'            => 'footer-right',
+			'description'   => esc_html__( 'Right Sidebar', 'alpha' ),
+			'before_widget' => '<div id="%1$s" class="footer-right %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+    register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Left Sidebar', 'alpha' ),
+			'id'            => 'footer-left',
+			'description'   => esc_html__( 'Left Sidebar', 'alpha' ),
+			'before_widget' => '<div id="%1$s" class="footer-left %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'alpha_sidebar' );
--- single.php
+++ single.php
@@ -1,44 +1,66 @@
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
 <?php get_template_part("hero"); ?>
-<div class="posts">
-    <?php 
-        while (have_posts()){
-            the_post();
-    ?>
-        <div class="post" <?php post_class(); ?>>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-10 offset-md-1">
-                        <h2 class="post-title text-center"><?php the_title();?></h2>
-                        <p class="text-center">
-                        <strong><?php the_author() ?></strong><br/>
-                            <?php echo get_the_date(); ?>
-                        </p>
-                    </div>
-                </div>
-                <div class="row">
-                <div class="col-md-10 offset-md-1">
-                    <p>
-                        <?php 
-                            if (has_post_thumbnail()){
-                                the_post_thumbnail("large", array("class" => "img-fluid"));
-                            }
-                        ?>
-                    </p>
-                    <?php 
-                        the_content();
-                    ?>
-                </div>
-                <?php if(comments_open()): ?>
-                    <div class="col-md-10 offset-md-1">
-                        <?php comments_template();?>
-                    </div>
-                <?php endif; ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="posts">
+                <?php 
+                    while (have_posts()){
+                        the_post();
+                ?>
+                    <div class="post" <?php post_class(); ?>>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-10 offset-md-1">
+                                    <h2 class="post-title text-center"><?php the_title();?></h2>
+                                    <p class="text-center">
+                                    <strong><?php the_author() ?></strong><br/>
+                                        <?php echo get_the_date(); ?>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-10 offset-md-1">
+                                    <p>
+                                        <?php 
+                                            if (has_post_thumbnail()){
+                                                the_post_thumbnail("large", array("class" => "img-fluid"));
+                                            }
+                                        ?>
+                                    </p>
+                                    <?php 
+                                        the_content();
+                                    ?>
+                                    <div class="row">
+                                        <div class="col-md-6 mt-3">
+                                            <?php previous_post_link() ?>
+                                        </div>
+                                        <div class="col-md-6 text-right">
+                                            <?php next_post_link(); ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php //if(comments_open()): ?>
+                                    <!-- <div class="col-md-10 offset-md-1"> -->
+                                        <?php //comments_template();?>
+                                    <!-- </div> -->
+                                <?php //endif; ?>
+                            </div>
+                        </div>
+                    </div>                
+                <?php
+                    }
+                ?>
             </div>
         </div>
-    <?php
-        }
-    ?>
+        <div class="col-md-4">
+            <?php
+                if(is_active_sidebar("sidebar-1")){
+                    dynamic_sidebar("sidebar-1");
+                }
+            ?>
+        </div>
+    </div>
 </div>
 <?php get_footer(); ?>
